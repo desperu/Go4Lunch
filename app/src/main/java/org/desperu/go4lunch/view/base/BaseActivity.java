c27732a8a162633b6979c25d1f152c8a3a0b96cc
desperu <motais.esteban@laposte.net>
2020-01-08 17:03:11 +0100

    Change back arrow action, to refresh map when back from settings activity, perfect refresh description

--- app/src/main/java/org/desperu/go4lunch/view/base/BaseActivity.java
+++ app/src/main/java/org/desperu/go4lunch/view/base/BaseActivity.java
@@ -1,8 +1,6 @@
 package org.desperu.go4lunch.view.base;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -10,12 +8,10 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.firebase.auth.FirebaseUser;
 
 import org.desperu.go4lunch.R;
 import org.desperu.go4lunch.viewmodel.UserAuthViewModel;
-import org.jetbrains.annotations.NotNull;
 
 import butterknife.ButterKnife;
 import icepick.Icepick;
@@ -49,19 +45,6 @@ public abstract class BaseActivity extends AppCompatActivity {
     }
 
     // --------------------
-    // ERROR HANDLER
-    // --------------------
-
-    protected OnFailureListener onFailureListener(){
-        return new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getApplicationContext(), getString(R.string.error_unknown_error), Toast.LENGTH_LONG).show();
-            }
-        };
-    }
-
-    // --------------------
     // UI
     // --------------------
 
@@ -73,16 +56,6 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void configureUpButton(){
         ActionBar ab = getSupportActionBar();
         if (ab != null) ab.setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NotNull MenuItem item) {
-        // Respond to the action bar's Up/Home button
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     // --------------------
